4d550bbc2ced5a7f92989c261c71e0830c4ca867
Rajneesh_Sharma25 <121484328+Rajneeshsharma125@users.noreply.github.com>
2024-07-20 16:37:22 +0000

    datatypes config done

---
+++ .devcontainer/01-basics/02-datatyes.js
@@ -0,0 +1,17 @@
+let name= "Rajneesh"
+let age = 21
+let isloggedIn= false
+
+
+// number=> 2 to the power 53
+// bigint
+// string=> ""
+// boolean => true/false
+// null => standalone value 
+// undefined =>
+    // symbol =>unique
+
+
+//  object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefiend
